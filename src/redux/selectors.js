44b1785e7d00a90a6b2366f1c0f15b0c168f7147
Ivan Grivtsov <ivangrivtsov0@gmail.com>
2024-03-14 10:25:03 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -4,4 +4,4 @@ export const selectFilters = state => state.filters;
 
 export const selectError = state => state.contacts.error;
 
-export const selectIsLoading = state => state.contacts.isLoading;
+export const selectIsLoading = state => state.contacts.loading;
